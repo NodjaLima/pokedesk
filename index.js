2762940c61130de9e88922553fbcc19759b6daed
NodjaLima <nodjamplima@gmail.com>
2022-05-08 18:37:59 -0300

    Primeiro commit

---
+++ index.js
@@ -0,0 +1,21 @@
+$('#submit').on('click', function(evento) {
+  evento.preventDefault()
+  var nome = $("#pk-name").val()
+  var url = `https://pokeapi.co/api/v2/pokemon/${nome}`
+
+  $.ajax({
+    url: url,
+    type: "GET",
+    dataType: 'json',
+
+    success: function(result) {
+      console.log(result)
+      $('#nome_pokemon').html(`This is ${result.name}`)
+      $('#img_pokemon').html(`<img src="${result.sprites.front_default}"/>`)
+      $('#habilidade_pokemon').html(`The best ability is ${result.abilities[1].ability.name}`)
+      $('#tipo_pokemon').html(`The default move is ${result.moves[0].move.name}`)
+    }
+
+  })
+})
+ 
